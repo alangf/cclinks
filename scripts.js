ef432e891bb5f2cce53a2fe6c4ad425a8f94078a
Alan Gomes <alangf@gmail.com>
2015-10-21 18:55:58 -0300

    inicial

---
+++ scripts.js
@@ -0,0 +1,21 @@
+var links;
+
+// links in post
+var post_content = document.getElementsByClassName("td-post-content");
+if (post_content.length > 0) {
+    var links = post_content[0].getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        links[i].setAttribute('target', '_blank')
+    }    
+}
+
+// links in comments
+var commments = document.getElementsByClassName("comment-content");
+for (var i = 0; i < comments.length; i++) {
+    var links = document.getElementsByTagName('a');
+    for (z = 0; z < links.length; z++) {
+        links[z].setAttribute('target', '_blank')
+    }
+
+}
+
